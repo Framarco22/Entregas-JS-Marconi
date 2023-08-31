f78550e21d2b91da5930e2391d19f897ddd05880
Framarco22 <francoemarconi@gmail.com>
2023-08-31 11:41:01 +0200

    primer commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const listaObjetos = [];
+
+function crearObjeto() {
+    const nombre = prompt("Ingrese el nombre del producto (Si quiere finalizar la lista, ingrese Fin):");
+
+    if (nombre.toLowerCase() === "fin") {
+        return false; 
+    } 
+const asdsa
+    let cantidad;
+    do {
+        const cantidadInput = prompt("Ingrese la cantidad o peso del producto:");
+        cantidad = cantidadInput;
+
+        if (isNaN(cantidad)) {
+            alert("La cantidad debe ser expresada en números. Por favor, ingrese la cantidad nuevamente.");
+        }
+    } while (isNaN(cantidad) || cantidad === 0);
+
+    const nuevoObjeto = {
+        nombre: nombre,
+        cantidad: cantidad
+    };
+
+    listaObjetos.push(nuevoObjeto);
+
+    return true; 
+}
+
+while (crearObjeto()) {
+
+}
+
+console.log("Lista de objetos:", listaObjetos);
+
