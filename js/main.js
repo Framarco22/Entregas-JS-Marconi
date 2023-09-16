23197c3fa5deca58e9b4225c492738514557e3df
Framarco22 <francoemarconi@gmail.com>
2023-09-16 16:35:57 +0200

    entrega 2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const listaObjetos = [];
 
 function crearObjeto() {
-    const nombre = prompt("Ingrese el nombre del producto (Si quiere finalizar la lista, ingrese Fin):");
+    const nombre = prompt("Ingrese el nombre del producto (Si quiere finalizar la lista, ingrese 'Fin'):");
+
+    if (nombre === null) {
+        return;
+    }
 
     if (nombre.toLowerCase() === "fin") {
-        return false; 
-    } 
+        return;
+    }
 
     let cantidad;
     do {
         const cantidadInput = prompt("Ingrese la cantidad o peso del producto:");
-        cantidad = cantidadInput;
+        cantidad = parseFloat(cantidadInput);
 
         if (isNaN(cantidad)) {
-            alert("La cantidad debe ser expresada en números. Por favor, ingrese la cantidad nuevamente.");
+            alert("La cantidad debe ser un número. Por favor, ingrese la cantidad nuevamente.");
         }
     } while (isNaN(cantidad) || cantidad === 0);
 
@@ -24,12 +28,23 @@ function crearObjeto() {
 
     listaObjetos.push(nuevoObjeto);
 
-    return true; 
+    alert(`Producto '${nombre}' agregado con cantidad ${cantidad}.`);
 }
 
-while (crearObjeto()) {
+function mostrarTabla() {
+    const objetoTabla = document.getElementById("tablaObjetos").getElementsByTagName('tbody')[0];
+    objetoTabla.innerHTML = "";
 
+    listaObjetos.forEach(function(objeto) {
+        const row = document.createElement("tr");
+        const cellNombre = document.createElement("td");
+        const cellCantidad = document.createElement("td");
+
+        cellNombre.textContent = objeto.nombre;
+        cellCantidad.textContent = objeto.cantidad;
+
+        row.appendChild(cellNombre);
+        row.appendChild(cellCantidad);
+        objetoTabla.appendChild(row);
+    });
 }
-
-console.log("Lista de objetos:", listaObjetos);
-
